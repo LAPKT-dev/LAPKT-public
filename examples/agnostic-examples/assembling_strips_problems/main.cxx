7ee063d195262b1cd32ca5394d4b183d9e028419
Miquel Ramirez <miquel.ramirez@gmail.com>
2012-08-24 10:32:17 +1000

    Examples re-estructured

--- examples/agnostic-examples/assembling_strips_problems/main.cxx
+++ examples/agnostic-examples/assembling_strips_problems/main.cxx
@@ -1,22 +1,16 @@
-// MRJ: In this example, we'll show how to create a search problem out of
-// a planning problem we acquired from some external source
-#include <sstream>
-#include <common/toy_graph.hxx>
-
+// MRJ: In this example, I'll try to show how can one assemble a simple
+// STRIPS planning problem from objects and data structures from an application.
 #include <strips_prob.hxx>
 #include <fluent.hxx>
 #include <action.hxx>
 #include <cond_eff.hxx>
-#include <strips_state.hxx>
+#include <sstream>
+#include <common/toy_graph.hxx>
 
-#include <fwd_search_prob.hxx>
+int main( int argc, char** argv ) {
 
-using	aptk::STRIPS_Problem;
-using	aptk::agnostic::Fwd_Search_Problem;
-using	aptk::State;
-using	aptk::Action;
-
-void setup_graph( Graph& g ) {
+	// MRJ: We create our graph
+	Graph	g;
 
 	g.add_vertex( "Kitchen" );	// v0
 	g.add_vertex( "Sitting_Room" );	// v1
@@ -28,10 +22,12 @@ void setup_graph( Graph& g ) {
 	g.connect( 1, 2 );
 	g.connect( 1, 3 );
 	g.connect( 1, 4 );
-}
 
-void setup_planning_problem( Graph& g, STRIPS_Problem& prob ) {
+	aptk::STRIPS_Problem prob;
 
+	// MRJ: Now we create one fluent for each of the possible locations, and keep
+	// the indices corresponding to each fluent associated with the appropiate
+	// graph vertex
 	std::map<unsigned, unsigned>	vtx_to_fl;
 
 	for ( unsigned v_k = 0; v_k < g.vertices().size(); v_k++ ) {
@@ -41,6 +37,7 @@ void setup_planning_problem( Graph& g, STRIPS_Problem& prob ) {
 		vtx_to_fl.insert( std::make_pair( v_k, fl_idx ) );
 	}
 
+	// MRJ: Actions in this task correspond to (directed) edges in the graph
 	for ( unsigned v_k = 0; v_k < g.vertices().size(); v_k++ )
 		for ( Graph::Vertex_It v_j = g.begin_adj( v_k );
 			v_j != g.end_adj( v_k ); v_j++ ) {
@@ -53,55 +50,39 @@ void setup_planning_problem( Graph& g, STRIPS_Problem& prob ) {
 			
 			buffer << "(move " << g.vertices()[v_k]->label() << " " << (*v_j)->label() << ")";
 
+			// MRJ: actions have as a precondition that agent is at location v_k
 			pre.push_back( vtx_to_fl[ v_k ] );
+			// MRJ: once the action is executed, the agent will now be at v_j
 			add.push_back( vtx_to_fl[ (*v_j)->index() ] );
+			// MRJ: and no longer where it was
 			del.push_back( vtx_to_fl[ v_k ] );
 
+			// MRJ: now we can register the action, but in this example we don't
+			// need to keep a reference to the action which results of this
 			aptk::STRIPS_Problem::add_action( prob, buffer.str(), pre, add, del, ceff );
 		}
 
+	// MRJ: After adding actions, it is necessary to initialize data structures that
+	// keep track of relationships between fluents and actions. These data structures
+	// are used to improve efficiency of search and heuristic computations.
+
 	prob.make_action_tables();
 
+	// MRJ: Now we can specify the initial and goal states
 	aptk::Fluent_Vec	I;
 	aptk::Fluent_Vec	G;
 	
+	// MRJ: The agent starts at the kitchen
 	I.push_back( vtx_to_fl[ 0 ] );
+	// MRJ: And wants to get to the balcony
 	G.push_back( vtx_to_fl[ 2 ] );
 
+	// MRJ: And now we set the initial and goal states of prob	
 	aptk::STRIPS_Problem::set_init( prob, I );
 	aptk::STRIPS_Problem::set_goal( prob, G );
 
-	prob.print( std::cout );
-}
-
-int main( int argc, char** argv ) {
-
-	// MRJ: We create our graph
-	Graph	g;
-	setup_graph( g );
-
-	STRIPS_Problem plan_prob;
-	setup_planning_problem( g, plan_prob );
-
-	Fwd_Search_Problem	search_prob( &plan_prob );
-
-	// MRJ: we get the initial state s_0
-	State* s_0 = search_prob.init();
-	
-	std::cout << "s_0 = ";
-	plan_prob.print_fluent_vec( std::cout, s_0->fluent_vec() );
-	std::cout << std::endl;
-
-	// MRJ: we can generate successors of s_0 via the iterator-based interface
-	for ( int ai = 0; ai < search_prob.num_actions(); ai++ ) {
-		if ( !search_prob.is_applicable( *s_0, ai ) ) continue; 
-		State* succ = search_prob.next( *s_0, ai );
-		std::cout << "s' = ";
-		plan_prob.print_fluent_vec( std::cout, succ->fluent_vec() );
-		std::cout << std::endl;
-		std::cout << "Reached executing action: " << std::endl;
-		plan_prob.actions().at( ai )->print( plan_prob, std::cout );
-	}
+	// MRJ: We'd be ready for solving this problem with a search algorithm
+	// but that's stuff to be covered by another example
 
 	return 0;
 }
--- examples/agnostic-examples/successor_generation/main.cxx
+++ examples/agnostic-examples/successor_generation/main.cxx
@@ -11,35 +11,12 @@
 #include <strips_state.hxx>
 
 #include <fwd_search_prob.hxx>
-#include <h_1.hxx>
-#include <aptk/open_list.hxx>
-#include <aptk/at_bfs.hxx>
 #include <aptk/string_conversions.hxx>
 
 using	aptk::STRIPS_Problem;
 using	aptk::State;
 using	aptk::Action;
 using	aptk::agnostic::Fwd_Search_Problem;
-using 	aptk::agnostic::H1_Heuristic;
-using	aptk::agnostic::H_Add_Evaluation_Function;
-using	aptk::agnostic::H_Max_Evaluation_Function;
-using 	aptk::search::Open_List;
-using	aptk::search::Node_Comparer;
-using	aptk::search::classical::AT_BFS_SQ_SH;
-using	aptk::search::classical::Node;
-
-// MRJ: We start defining the type of nodes for our planner
-typedef		Node< State >							Search_Node;
-// MRJ: Then we define the type of the tie-breaking algorithm
-// for the open list we are going to use
-typedef		Node_Comparer< Search_Node >					Tie_Breaking_Algorithm;
-// MRJ: Now we define the Open List type by combining the types we have defined before
-typedef		Open_List< Tie_Breaking_Algorithm, Search_Node >		BFS_Open_List;
-// MRJ: Now we define the heuristic
-typedef		H1_Heuristic<Fwd_Search_Problem, H_Max_Evaluation_Function>	H_Max_Fwd;
-typedef		H1_Heuristic<Fwd_Search_Problem, H_Add_Evaluation_Function>	H_Add_Fwd;
-// MRJ: Now we're ready to define the BFS algorithm we're going to use
-typedef		AT_BFS_SQ_SH< Fwd_Search_Problem, H_Add_Fwd, BFS_Open_List >	Anytime_BFS;
 
 int main( int argc, char** argv ) {
 
@@ -48,8 +25,8 @@ int main( int argc, char** argv ) {
 	int	n_items = 12;
 	int	n_goal_items = 5;
 	int 	n_goal_locs = 4;
+	int 	i = 1;
 
-	int i = 1;
 	while ( i < argc ) {
 		std::string parm = argv[i];
 		if ( parm == "--dim" ) {
@@ -115,44 +92,32 @@ int main( int argc, char** argv ) {
 	plan_prob.print_fluent_vec( std::cout, plan_prob.goal() );
 	std::cout << std::endl;
 
-	std::cout << "Starting search..." << std::endl;
 
 	Fwd_Search_Problem	search_prob( &plan_prob );
-	Anytime_BFS		search_engine( search_prob );
 
-	search_engine.start();
-
-	std::vector< aptk::Action_Idx > plan;
-	float				cost;
-
-	float 		ref = aptk::time_used();
-	float t0 = aptk::time_used();
-	unsigned expanded_0 = search_engine.expanded();
-	unsigned generated_0 = search_engine.generated();
-	while ( search_engine.find_solution( cost, plan ) ) {
-		std::cout << "Plan found with cost: " << cost << std::endl;
-		for ( unsigned k = 0; k < plan.size(); k++ ) {
-			std::cout << k+1 << ". ";
-			const Action& a = *(plan_prob.actions()[ plan[k] ]);
-			std::cout << a.signature();
-			std::cout << std::endl;
-		}
-		float tf = aptk::time_used();
-		unsigned expanded_f = search_engine.expanded();
-		unsigned generated_f = search_engine.generated();
-		std::cout << "Time: " << tf - t0 << std::endl;
-		std::cout << "Generated: " << generated_f - generated_0 << std::endl;
-		std::cout << "Expanded: " << expanded_f - expanded_0 << std::endl;
-		t0 = tf;
-		expanded_0 = expanded_f;
-		generated_0 = generated_f;
+	// MRJ: Example of different successor generator modalities
+	std::cout << "Applicable actions at root by direct checking: " << std::endl;
+	State* s0 = search_prob.init();
+	
+	for ( int i = 0; i < search_prob.num_actions(); i++ ) {
+		if ( search_prob.is_applicable( *s0, i ) ) 
+			std::cout << plan_prob.actions()[i]->signature() << std::endl;		
 	}
-	std::cout << "Total time: " << aptk::time_used() - ref << std::endl;
-	std::cout << "Nodes generated during search: " << search_engine.generated() << std::endl;
-	std::cout << "Nodes expanded during search: " << search_engine.expanded() << std::endl;
-	std::cout << "Nodes pruned by bound: " << search_engine.pruned_by_bound() << std::endl;
-	std::cout << "Dead-end nodes: " << search_engine.dead_ends() << std::endl;
-	std::cout << "Nodes in OPEN replaced: " << search_engine.open_repl() << std::endl;
+
+	std::cout << "Applicable actions at root with successor generator: " << std::endl;
+	std::vector< aptk::Action_Idx > app_set;
+	search_prob.applicable_set( *s0, app_set );
+	for ( int i = 0; i < app_set.size(); i++ ) {
+		std::cout << plan_prob.actions()[app_set[i]]->signature() << std::endl;		
+	}
+
+	std::cout << "Applicable actions at root with iterator interface: " << std::endl;
+	Fwd_Search_Problem::Action_Iterator it(search_prob);
+	int a = it.start( *s0 );
+	while (a != aptk::no_op) {
+		std::cout << plan_prob.actions()[a]->signature() << std::endl;	
+		a = it.next();
+	}
 
 	return 0;
 }
