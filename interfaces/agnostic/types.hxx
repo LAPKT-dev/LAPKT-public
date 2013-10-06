6178c7f4472d3a333157efde813e1354d5a4a86f
Miquel Ramirez <miquel.ramirez@gmail.com>
2013-10-06 14:14:39 +1100

    Tabs

--- interfaces/agnostic/types.hxx
+++ interfaces/agnostic/types.hxx
@@ -38,13 +38,13 @@ namespace aptk
 	typedef		std::vector<unsigned>				Fluent_Vec;
 	typedef		std::vector<unsigned>				Index_Vec;
 	typedef		std::vector<float>				Value_Vec;
-	typedef		std::pair<unsigned, unsigned>		Fluent_Pair;
+	typedef		std::pair<unsigned, unsigned>			Fluent_Pair;
 	typedef		Bit_Set						Fluent_Set;
-	typedef 		std::vector<Action* >				Action_Ptr_Vec;
-	typedef 		std::vector<Fluent* >				Fluent_Ptr_Vec;
+	typedef 	std::vector<Action* >				Action_Ptr_Vec;
+	typedef 	std::vector<Fluent* >				Fluent_Ptr_Vec;
 	typedef		std::vector<Conditional_Effect* > 		Conditional_Effect_Vec;
 	typedef		std::vector< std::vector< const Action* > >	Fluent_Action_Table;
-	typedef         	std::vector< Action_Ptr_Vec >           	PDDLop_Action_Table;
+	typedef         std::vector< Action_Ptr_Vec >           	PDDLop_Action_Table;
 	typedef		std::vector< Fluent_Ptr_Vec >	        	Type_Fluent_Table;
 	typedef		std::vector< Fluent_Ptr_Vec >	        	Object_Fluent_Table;
 
