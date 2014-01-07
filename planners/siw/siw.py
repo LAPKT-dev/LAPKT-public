dc35cfb0b032f942372922c378dce314ce71a795
Miquel Ramirez <miquel.ramirez@gmail.com>
2014-01-07 13:39:53 +1100

    Changed name of module

--- planners/siw/siw.py
+++ planners/siw/siw.py
@@ -1,15 +1,19 @@
 #! /usr/bin/env python
 import fd.grounding
 import sys
-from libaptk import FOD_Problem
+from libsiw import STRIPS_Problem
 
 def main( domain_file, problem_file) :
-	task = FOD_Problem( )
+	task = STRIPS_Problem( )
 
 	fd.grounding.default( domain_file, problem_file, task )
 
 	for i in range( 0, task.num_actions() ) :
 		task.print_action( i )
+
+def debug() :
+	main( "/home/bowman/Sandboxes/Fast-Downward/benchmarks/miconic-simpleadl/domain.pddl",
+		"/home/bowman/Sandboxes/Fast-Downward/benchmarks/miconic-simpleadl/s3-0.pddl" )
 
 if __name__ == "__main__":
 	main( sys.argv[1], sys.argv[2])
