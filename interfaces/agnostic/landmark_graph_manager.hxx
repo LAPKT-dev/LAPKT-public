48ffc887e5c6771c16725471719c96fcd3f1ef5e
Nir Lipovetzky <nirlipo@gmail.com>
2014-09-30 12:03:10 +1000

    new interface for land count and manager

--- interfaces/agnostic/landmark_graph_manager.hxx
+++ interfaces/agnostic/landmark_graph_manager.hxx
@@ -175,6 +175,88 @@ public:
 		}
 	}
 
+	void apply_action( State* s, Action_Idx a_idx ){
+		const Action* a = m_strips_model.actions()[ a_idx ];
+		const Fluent_Vec& add = a->add_vec();
+		const Fluent_Vec& del = a->del_vec();		
+
+		for(Fluent_Vec::const_iterator it_add = add.begin(); it_add != add.end(); it_add++){
+			unsigned p = *it_add;
+			
+			if( m_graph->is_landmark(p) ){
+				Landmarks_Graph::Node* n = m_graph->node(p);
+				if( !n->is_consumed() )
+					if( n->are_precedences_consumed()  && n->are_gn_precedences_consumed() ){
+					    n->consume( );
+					}
+			}
+		}
+
+		for(Fluent_Vec::const_iterator it_del = del.begin(); it_del != del.end(); it_del++){
+			unsigned p = *it_del;
+						
+			if( m_graph->is_landmark(p) ){
+				bool unconsume = false;
+				Landmarks_Graph::Node* n = m_graph->node(p);
+
+				if( n->is_consumed() )
+					if(  m_strips_model.is_in_goal(p) || (! n->are_requirements_consumed() ) || (! n->are_gn_requirements_consumed() ) )
+						unconsume = true;
+				
+
+				if(unconsume){
+					n->unconsume( );
+				}
+			}
+		}
+
+		if( !a->ceff_vec().empty() ){		
+			for( unsigned i = 0; i < a->ceff_vec().size(); i++ ){
+				Conditional_Effect* ce = a->ceff_vec()[i];
+				if( ce->can_be_applied_on( *s ) )
+					apply_cond_eff(s,ce);
+			}
+		}
+
+	}
+
+	void apply_cond_eff( State* s, Conditional_Effect* ce ){
+
+	  
+		const Fluent_Vec& add = ce->add_vec();
+		const Fluent_Vec& del = ce->del_vec();		
+
+		for(Fluent_Vec::const_iterator it_add = add.begin(); it_add != add.end(); it_add++){
+			unsigned p = *it_add;
+			
+			if( m_graph->is_landmark(p) ){
+				Landmarks_Graph::Node* n = m_graph->node(p);
+				if( !n->is_consumed() )
+					if( n->are_precedences_consumed()  && n->are_gn_precedences_consumed() ){
+						n->consume( );
+					}
+			}
+		}
+
+		for(Fluent_Vec::const_iterator it_del = del.begin(); it_del != del.end(); it_del++){
+			unsigned p = *it_del;
+						
+			if( m_graph->is_landmark(p) ){
+				bool unconsume = false;
+				Landmarks_Graph::Node* n = m_graph->node(p);
+
+				if( n->is_consumed() )
+					if(  m_strips_model.is_in_goal(p) || (! n->are_requirements_consumed() ) || (! n->are_gn_requirements_consumed() ) )
+						unconsume = true;
+				
+
+				if(unconsume){
+					n->unconsume( );
+				}
+			}
+		}
+	}
+
 	void apply_state( const Fluent_Vec& fl, Bool_Vec_Ptr*& keep_consumed, Bool_Vec_Ptr*& keep_unconsumed ){
 
 		for(Fluent_Vec::const_iterator it_fl = fl.begin(); it_fl != fl.end(); it_fl++){
@@ -187,6 +269,22 @@ public:
 					if( !keep_consumed ) keep_consumed = new Bool_Vec_Ptr;
 					n->consume( );
 					keep_consumed->push_back( n->is_consumed_ptr() );
+				}
+			}
+		}
+
+	}
+
+	void apply_state( const Fluent_Vec& fl ){
+
+		for(Fluent_Vec::const_iterator it_fl = fl.begin(); it_fl != fl.end(); it_fl++){
+			unsigned p = *it_fl;
+
+			if( m_graph->is_landmark(p) ){
+				
+				Landmarks_Graph::Node* n = m_graph->node(p);
+				if( (! n->is_consumed()) && n->are_precedences_consumed() && n->are_gn_precedences_consumed() ){
+					n->consume( );	
 				}
 			}
 		}
