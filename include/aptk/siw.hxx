93838f635116bce2e1a186a081f6c68dd05b226d
Miquel Ramirez <miquel.ramirez@gmail.com>
2016-05-16 17:08:22 +1000

    Compile Error fixed
    * Added missing include directive, thanks to D. Maggi for the report.

--- include/aptk/siw.hxx
+++ include/aptk/siw.hxx
@@ -25,6 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <aptk/resources_control.hxx>
 #include <aptk/closed_list.hxx>
 #include <aptk/serialized_search.hxx>
+#include <aptk/iw.hxx>
 #include <landmark_graph.hxx>
 #include <vector>
 #include <algorithm>
@@ -36,8 +37,8 @@ namespace search {
 
 
 template < typename Search_Model >
-class SIW : public Serialized_Search<Search_Model, 
-				     brfs::IW<Search_Model, 
+class SIW : public Serialized_Search<Search_Model,
+				     brfs::IW<Search_Model,
 					      aptk::agnostic::Novelty<Search_Model, aptk::search::brfs::Node< aptk::State >>>,
 				     aptk::search::brfs::Node< aptk::State >> {
 
@@ -46,8 +47,8 @@ public:
 	typedef  	 aptk::search::brfs::Node< aptk::State >					Search_Node;
 	typedef          aptk::agnostic::Landmarks_Graph                                                Landmarks_Graph;
 
-	SIW( const Search_Model& search_problem ) 
-		: Serialized_Search<Search_Model, brfs::IW<Search_Model, aptk::agnostic::Novelty<Search_Model, Search_Node>>, Search_Node>( search_problem ), m_pruned_sum_B_count(0), m_sum_B_count(0), m_max_B_count(0), m_iw_calls(0), m_max_bound( std::numeric_limits<unsigned>::max() ) {	   	
+	SIW( const Search_Model& search_problem )
+		: Serialized_Search<Search_Model, brfs::IW<Search_Model, aptk::agnostic::Novelty<Search_Model, Search_Node>>, Search_Node>( search_problem ), m_pruned_sum_B_count(0), m_sum_B_count(0), m_max_B_count(0), m_iw_calls(0), m_max_bound( std::numeric_limits<unsigned>::max() ) {
 		m_goal_agenda = NULL;
 	}
 
@@ -56,13 +57,13 @@ public:
 	void            set_goal_agenda( Landmarks_Graph* lg ) { m_goal_agenda = lg; }
 
 	virtual bool	find_solution( float& cost, std::vector<Action_Idx>& plan ) {
-		
+
 		unsigned gsize = this->problem().task().goal().size();
 		Search_Node* end = NULL;
 		State* new_init_state = NULL;
 		this->m_goals_achieved.clear();
 		this->m_goal_candidates.clear();
-		
+
 		cost = 0;
 
 		if ( this->verbose() )
@@ -72,14 +73,14 @@ public:
 			m_goal_agenda->get_leafs( this->m_goal_candidates );
 		}
 		else{
-			this->m_goal_candidates.insert( this->m_goal_candidates.begin(), 
+			this->m_goal_candidates.insert( this->m_goal_candidates.begin(),
 							this->problem().task().goal().begin(), this->problem().task().goal().end() );
 		}
 
 		do{
-			if ( this->verbose() )	
+			if ( this->verbose() )
 				std::cout << std::endl << "{" << gsize << "/" << this->m_goal_candidates.size() << "/" << this->m_goals_achieved.size() << "}:IW(" << this->bound() << ") -> ";
-			end = this->do_search();		
+			end = this->do_search();
 			m_pruned_sum_B_count += this->pruned_by_bound();
 
 			if ( end == NULL ) {
@@ -94,16 +95,16 @@ public:
 				 */
 				if( this->pruned_by_bound() == 0)
 					return false;
-				
+
 				new_init_state = new State( this->problem().task() );
 				new_init_state->set( this->m_root->state()->fluent_vec() );
-				new_init_state->update_hash();				
+				new_init_state->update_hash();
 
 				if ( this->bound() > this->max_bound() ) // Hard cap on width exceeded
 					return false;
 
-				this->set_bound( this->bound()+1 );				
-				this->start( new_init_state );				
+				this->set_bound( this->bound()+1 );
+				this->start( new_init_state );
 
 				// Memory exceeded to reserve data structures for novelty
 				if(this->m_novelty->arity() != this->bound() )
@@ -116,23 +117,23 @@ public:
 				 * IW(1) is thrown from end_state
 				 */
 
-				m_max_B_count = m_max_B_count < this->bound() ? this->bound() : m_max_B_count; 
+				m_max_B_count = m_max_B_count < this->bound() ? this->bound() : m_max_B_count;
 				m_sum_B_count += this->bound();
 				m_iw_calls++;
 
 				std::vector<Action_Idx> partial_plan;
 				float partial_cost = 0.0f;
-				this->extract_plan( this->m_root, end, partial_plan, partial_cost );	
-				plan.insert( plan.end(), partial_plan.begin(), partial_plan.end() );			
-				cost += partial_cost;				
+				this->extract_plan( this->m_root, end, partial_plan, partial_cost );
+				plan.insert( plan.end(), partial_plan.begin(), partial_plan.end() );
+				cost += partial_cost;
 
 				new_init_state = new State( this->problem().task() );
 				new_init_state->set( end->state()->fluent_vec() );
 				new_init_state->update_hash();
-				
+
 				this->set_bound( 1 );
 				this->start( new_init_state );
-				
+
 
 				if( m_goal_agenda ){
 					for(Fluent_Vec::iterator it = this->m_goals_achieved.begin(); it != this->m_goals_achieved.end(); it++)
@@ -140,20 +141,20 @@ public:
 
 					this->m_goal_candidates.clear();
 					m_goal_agenda->get_leafs( this->m_goal_candidates );
-					
+
 				}
-				
+
 
 				//this->debug_info( new_init_state, this->m_goal_candidates );
-				
-				
+
+
 			}
 
 
 		}while( !this->problem().goal( *new_init_state ) );
 
 
-		
+
 		return true;
 	}
 
@@ -163,7 +164,7 @@ public:
 	void			set_max_bound( unsigned v ) { m_max_bound = v; }
 	unsigned		max_bound( ) { return m_max_bound; }
 
-protected:	       
+protected:
 	unsigned		m_pruned_sum_B_count;
 	unsigned		m_sum_B_count;
 	unsigned		m_max_B_count;
@@ -173,7 +174,7 @@ protected:
 };
 
 }
-		
+
 }
 
 
