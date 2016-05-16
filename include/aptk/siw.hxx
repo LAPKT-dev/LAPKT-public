6d5ce1cddb5a0956d2fef997199ec31551292a54
Miquel Ramirez <miquel.ramirez@gmail.com>
2016-05-16 17:11:23 +1000

    Missing #include directive
    * Thanks to D. Maggi for the report

--- include/aptk/siw.hxx
+++ include/aptk/siw.hxx
@@ -24,6 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <aptk/search_prob.hxx>
 #include <aptk/resources_control.hxx>
 #include <aptk/closed_list.hxx>
+#include <aptk/iw.hxx>
 #include <aptk/serialized_search.hxx>
 #include <landmark_graph.hxx>
 #include <vector>
