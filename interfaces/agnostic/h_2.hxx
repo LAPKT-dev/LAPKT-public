08a34e26f459f23d55f680a5758ba47ca90df592
Nir Lipovetzky <nir.lipovetzky@unimelb.edu.au>
2020-04-07 12:47:16 +1000

    missing bracket

--- interfaces/agnostic/h_2.hxx
+++ interfaces/agnostic/h_2.hxx
@@ -669,7 +669,7 @@ protected:
 
 								//if anyone of the r,s pair can not be set to 0 when consider the relevant_actions, we should break.
                                 if (h2_pre_noop == infty )  break;
-                                 
+							} 
 						}
 						
 						if ( h2_pre_noop == infty ) continue;
