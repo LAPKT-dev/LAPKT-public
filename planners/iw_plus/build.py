7a4bdb08b30bc8bec2ee1e666dd4b60bcebb3f36
Nir Lipovetzky <nirlipo@gmail.com>
2020-06-09 23:19:55 +1000

    other builds

--- planners/iw_plus/build.py
+++ planners/iw_plus/build.py
@@ -7,20 +7,22 @@ LWAPTK_ROOT = '../..'
 def main() :
 	
 	# 1. Copy FD python parsing files
-	os.system( 'rm -rf fd' )
-	rv = os.system( 'cp -ra %(LWAPTK_ROOT)s/external/fd .'%globals() )
+	os.system( 'rm -Rf fd' )
+	rv = os.system( 'cp -Ra %(LWAPTK_ROOT)s/external/fd .'%globals() )
 	if rv != 0 :
-		print >> sys.stderr, "Could not copy FD parser files!"
+		sys.stderr.write("Could not copy FD parser files!")
 		sys.exit(1)
 
 	# 2. Call scons to build
-        if len(sys.argv) > 1:
-                rv = os.system( "scons %s" % sys.argv[1] )
-        else:
-	        rv = os.system( 'scons -j 8' )
+	if len(sys.argv) > 1:
+			rv = os.system( "scons %s" % sys.argv[1] )
+	else:
+		rv = os.system( 'scons -j 8' )
+
 	if rv != 0 :
-		print >> sys.stderr, "Build failed!"
+		sys.stderr.write("Build failed!")
 		sys.exit(1)
+
 
 if __name__ == '__main__' :
 	main()
