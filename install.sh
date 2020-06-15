bf44d227ddeeb433c370de118ed39ab2ef8bcc61
anubhav-cs <anubhav.singh.eng@gmail.com>
2020-06-15 19:10:21 +1000

    install script minor changes

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cmake --clean-first -Bbuild -S./src -DCMAKE_INSTALL_PREFIX=release
-cmake --build build -j$(grep -c ^processor /proc/cpuinfo) 
+cmake --clean-first -B build -S ./src -DCMAKE_INSTALL_PREFIX=release
+cmake --build build -j $(grep -c ^processor /proc/cpuinfo) 
 cmake --install build --component Development --prefix release
 
