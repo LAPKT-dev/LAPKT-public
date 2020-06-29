28abe568b04b0b2d447b3a813f964756f5e6db91
anubhav-cs <anubhav.singh.eng@gmail.com>
2020-06-30 04:42:22 +1000

    Adding clingo/gringo wrapper, which is a dependency of tarski grounder, to act as an python program that call the actual gringo executable

--- src/config/SuperBuild.cmake
+++ src/config/SuperBuild.cmake
@@ -6,7 +6,12 @@ include(ExternalProject)
 set(DEPENDENCIES)
 set(EXTRA_CMAKE_ARGS)
 
-list(APPEND DEPENDENCIES external_boost)
+list(APPEND DEPENDENCIES 
+    external_boost 
+    external_clingo_linux 
+    external_clingo_win32 
+    external_clingo_darwin
+    )
 
 if(CMAKE_BOOST_BUILD_TYPE STREQUAL "Debug")
     set(BOOST_VARIANT "debug")
@@ -14,6 +19,39 @@ else()
     set(BOOST_VARIANT "release")
 endif()
 message("Building Boost in " ${BOOST_VARIANT} " mode")
+
+ExternalProject_Add( external_clingo_linux
+    URL https://github.com/potassco/clingo/releases/download/v5.4.0/clingo-5.4.0-linux-x86_64.tar.gz
+    URL_MD5 d8e5767d1f482ddfc98d010191422af8
+    DOWNLOAD_DIR ${CMAKE_SOURCE_DIR}/external
+    SOURCE_DIR  ${CMAKE_BINARY_DIR}/../clingo/linux
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND "" # ./b2 install
+    )
+
+ExternalProject_Add( external_clingo_darwin
+    URL https://github.com/potassco/clingo/releases/download/v5.4.0/clingo-5.4.0-macos-x86_64.tar.gz
+    URL_MD5 64b46fde3a75e02368c25cd9f2d37029
+    DOWNLOAD_DIR ${CMAKE_SOURCE_DIR}/external
+    SOURCE_DIR  ${CMAKE_BINARY_DIR}/../clingo/darwin
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND "" # ./b2 install
+    )
+
+ExternalProject_Add( external_clingo_win32
+    URL https://github.com/potassco/clingo/releases/download/v5.4.0/clingo-5.4.0-win64.zip
+    URL_MD5 61815445476e20d4ce4f00060626d2da
+    DOWNLOAD_DIR ${CMAKE_SOURCE_DIR}/external
+    SOURCE_DIR  ${CMAKE_BINARY_DIR}/../clingo/win32
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND "" # ./b2 install
+    )
 
 ExternalProject_Add( external_boost
     URL https://dl.bintray.com/boostorg/release/1.73.0/source/boost_1_73_0.tar.bz2
