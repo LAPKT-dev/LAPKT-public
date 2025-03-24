73259ca3262ba2b00671106711d715ce57716a9a
Anubhav Singh <anubhav.singh.eng@gmail.com>
2025-03-24 17:54:59 -0400

    Python wrapper formatting and license
    Added license text and reformatted the source code

--- src/wrapper/py_strips_interface.hxx
+++ src/wrapper/py_strips_interface.hxx
@@ -1,3 +1,32 @@
+/**
+ * @file py_strips_interface.hxx
+ * @author Anubhav Singh (anubhav.singh.er@pm.me)
+ * @brief 
+ * @version 0.1
+ * @date 2023-03-28
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files
+  (the "Software"), to deal in the Software without restriction,
+  including without limitation the rights to use, copy, modify, merge,
+  publish, distribute, sublicense, and/or sell copies of the Software,
+  and to permit persons to whom the Software is furnished to do so, subject
+  to the following conditions:
+
+  The above copyright notice and this permission notice shall be included
+  in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #ifndef __PY_FOD_PROBLEM__
 #define __PY_FOD_PROBLEM__
 #define BOOST_BIND_GLOBAL_PLACEHOLDERS
@@ -15,81 +44,83 @@ namespace py = pybind11;
 using namespace py::literals;
 
 // class PYBIND11_EXPORT STRIPS_Interface {
-class PYBIND11_EXPORT STRIPS_Interface {
+class PYBIND11_EXPORT STRIPS_Interface
+{
 public:
 	STRIPS_Interface();
 	STRIPS_Interface(std::string, std::string);
 	virtual ~STRIPS_Interface();
 
-	aptk::STRIPS_Problem*	instance(){
+	aptk::STRIPS_Problem *instance()
+	{
 		return m_problem;
 	}
-    // aptk::STRIPS_Problem&	_instance(){
+	// aptk::STRIPS_Problem&	_instance(){
 	// 	return *m_problem;
 	// }
-	aptk::Fluent_Vec&		get_init()	  			{ return m_problem->init(); }
-	aptk::Fluent_Vec&		get_goal()	  			{ return m_problem->goal(); }
-	void	add_atom(std::string name);
-	void	add_action(std::string name, bool flag_tarski=false);
-	void	add_precondition(int index,
-		 std::vector<std::pair<int, bool>>& lits);
-	void	add_precondition(int index, py::list& lits);
-    // For Tarski
-	void	add_cond_effect(int index, std::vector<std::pair<int, bool>>&
-		cond_lits, std::vector<std::pair<int, bool>>& eff_lits);
-    // For FD
-	void	add_cond_effect(int index, py::list& cond_lits,
-		py::list& eff_lits);
-    // For Tarski
-	void	add_effect(int index, std::vector<std::pair<int, bool>>& list);
-    // For FD
-	void	add_effect(int index, py::list& list);
-	void	set_cost(int index, float v);
-    void    finalize_actions();
+	aptk::Fluent_Vec &get_init() { return m_problem->init(); }
+	aptk::Fluent_Vec &get_goal() { return m_problem->goal(); }
+	void add_atom(std::string name);
+	void add_action(std::string name, bool flag_tarski = false);
+	void add_precondition(int index,
+												std::vector<std::pair<int, bool>> &lits);
+	void add_precondition(int index, py::list &lits);
+	// For Tarski
+	void add_cond_effect(int index, std::vector<std::pair<int, bool>> &cond_lits, std::vector<std::pair<int, bool>> &eff_lits);
+	// For FD
+	void add_cond_effect(int index, py::list &cond_lits,
+											 py::list &eff_lits);
+	// For Tarski
+	void add_effect(int index, std::vector<std::pair<int, bool>> &list);
+	// For FD
+	void add_effect(int index, py::list &list);
+	void set_cost(int index, float v);
+	void finalize_actions();
 
-	virtual
-	void	add_mutex_group(py::list& list);
-    // For FD
-	void	notify_negated_conditions(py::list& list);
-    // For Tarski
-	void	notify_negated_atom(unsigned& fl_idx);
-	void	create_negated_fluents();
-    void    set_size_negated_fluents(size_t size) ;
+	virtual void add_mutex_group(py::list &list);
+	// For FD
+	void notify_negated_conditions(py::list &list);
+	// For Tarski
+	void notify_negated_atom(unsigned &fl_idx);
+	void create_negated_fluents();
+	void set_size_negated_fluents(size_t size);
 
-    // For Tarski
-	void	set_init(std::vector<std::pair<int, bool>>& list);
-    // For FD
-	void	set_init(py::list& list);
-    // For Tarski
-	void	set_goal(std::vector<std::pair<int, bool>>& list);
-	void	set_goal(py::list& list);
-	void	set_domain_name(std::string name);
-	void	set_problem_name(std::string name);
+	// For Tarski
+	void set_init(std::vector<std::pair<int, bool>> &list);
+	// For FD
+	void set_init(py::list &list);
+	// For Tarski
+	void set_goal(std::vector<std::pair<int, bool>> &list);
+	void set_goal(py::list &list);
+	void set_domain_name(std::string name);
+	void set_problem_name(std::string name);
 
-	virtual	void	setup(bool gen_match_tree=true);
+	virtual void setup(bool gen_match_tree = true);
 
-	void	print_action(int index);
+	void print_action(int index);
 
-	void	print_fluents();
-	void	print_actions();
+	void print_fluents();
+	void print_actions();
 
-	std::string	get_atom_name(int idx) const {
-		return m_problem->fluents()[idx]->signature(); }
-	std::string	get_domain_name() const { return m_problem->domain_name(); }
-	std::string	get_problem_name() const { return m_problem->problem_name(); }
+	std::string get_atom_name(int idx) const
+	{
+		return m_problem->fluents()[idx]->signature();
+	}
+	std::string get_domain_name() const { return m_problem->domain_name(); }
+	std::string get_problem_name() const { return m_problem->problem_name(); }
 
-	size_t	n_atoms() 	const { return m_problem->num_fluents(); }
-	size_t	n_actions()	const { return m_problem->num_actions(); }
+	size_t n_atoms() const { return m_problem->num_fluents(); }
+	size_t n_actions() const { return m_problem->num_actions(); }
 
-	void	write_ground_pddl(std::string domain, std::string instance);
+	void write_ground_pddl(std::string domain, std::string instance);
 
-	float	m_parsing_time;
-	bool	m_ignore_action_costs;
+	float m_parsing_time;
+	bool m_ignore_action_costs;
 
 protected:
-	aptk::STRIPS_Problem*	m_problem;
-	std::set<int>		m_negated_conditions;
-	aptk::Fluent_Ptr_Vec	m_negated;
+	aptk::STRIPS_Problem *m_problem;
+	std::set<int> m_negated_conditions;
+	aptk::Fluent_Ptr_Vec m_negated;
 };
 
 #endif // py_strips_problem.hxx
--- src/wrapper/pybind11_module.cxx
+++ src/wrapper/pybind11_module.cxx
@@ -1,3 +1,32 @@
+/**
+ * @file pybind11_module.hxx
+ * @author Anubhav Singh (anubhav.singh.er@pm.me)
+ * @brief 
+ * @version 0.1
+ * @date 2023-03-28
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files
+  (the "Software"), to deal in the Software without restriction,
+  including without limitation the rights to use, copy, modify, merge,
+  publish, distribute, sublicense, and/or sell copies of the Software,
+  and to permit persons to whom the Software is furnished to do so, subject
+  to the following conditions:
+
+  The above copyright notice and this permission notice shall be included
+  in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include <py_strips_interface.hxx>
 #include <h_1.hxx>
 #include <h_1_callback.hxx>
@@ -23,58 +52,30 @@ PYBIND11_MODULE(wrapper, m)
         .def("get_atom_name", &STRIPS_Interface::get_atom_name)
         .def("get_domain_name", &STRIPS_Interface::get_domain_name)
         .def("get_problem_name", &STRIPS_Interface::get_problem_name)
-        .def("add_precondition", 
-            static_cast<void (STRIPS_Interface::*) 
-                (int, std::vector<std::pair<int, bool>>&)>
-            (&STRIPS_Interface::add_precondition)
-        )
         .def("add_precondition",
-            static_cast<void (STRIPS_Interface::*) (int, py::list&)>
-            (&STRIPS_Interface::add_precondition)
-        )
-        .def("add_effect", 
-            static_cast<void (STRIPS_Interface::*)
-                (int, std::vector<std::pair<int, bool>>&)>
-            (&STRIPS_Interface::add_effect)
-        )
+             static_cast<void (STRIPS_Interface::*)(int, std::vector<std::pair<int, bool>> &)>(&STRIPS_Interface::add_precondition))
+        .def("add_precondition",
+             static_cast<void (STRIPS_Interface::*)(int, py::list &)>(&STRIPS_Interface::add_precondition))
         .def("add_effect",
-            static_cast<void (STRIPS_Interface::*) (int, py::list&)>
-            (&STRIPS_Interface::add_effect)
-        )
+             static_cast<void (STRIPS_Interface::*)(int, std::vector<std::pair<int, bool>> &)>(&STRIPS_Interface::add_effect))
+        .def("add_effect",
+             static_cast<void (STRIPS_Interface::*)(int, py::list &)>(&STRIPS_Interface::add_effect))
         .def("add_cond_effect",
-            static_cast<void (STRIPS_Interface::*)
-                (int, std::vector<std::pair<int, bool>>&,
-                std::vector<std::pair<int, bool>>&)>
-            (&STRIPS_Interface::add_cond_effect)
-        )
+             static_cast<void (STRIPS_Interface::*)(int, std::vector<std::pair<int, bool>> &,
+                                                    std::vector<std::pair<int, bool>> &)>(&STRIPS_Interface::add_cond_effect))
         .def("add_cond_effect",
-            static_cast<void (STRIPS_Interface::*)
-                (int, py::list&, py::list&)>
-            (&STRIPS_Interface::add_cond_effect)
-        )
+             static_cast<void (STRIPS_Interface::*)(int, py::list &, py::list &)>(&STRIPS_Interface::add_cond_effect))
         .def("set_cost", &STRIPS_Interface::set_cost)
         .def("notify_negated_conditions", &STRIPS_Interface::notify_negated_conditions)
         .def("create_negated_fluents", &STRIPS_Interface::create_negated_fluents)
-        .def("set_init", 
-            static_cast<void (STRIPS_Interface::*)
-                (std::vector<std::pair<int, bool>>&)>
-            (&STRIPS_Interface::set_init)
-        )
         .def("set_init",
-            static_cast<void (STRIPS_Interface::*)
-                (py::list&)>
-            (&STRIPS_Interface::set_init)
-        )
-        .def("set_goal", 
-            static_cast<void (STRIPS_Interface::*)
-                (std::vector<std::pair<int, bool>>&)>
-            (&STRIPS_Interface::set_goal)
-        )
+             static_cast<void (STRIPS_Interface::*)(std::vector<std::pair<int, bool>> &)>(&STRIPS_Interface::set_init))
+        .def("set_init",
+             static_cast<void (STRIPS_Interface::*)(py::list &)>(&STRIPS_Interface::set_init))
         .def("set_goal",
-            static_cast<void (STRIPS_Interface::*)
-                (py::list&)>
-            (&STRIPS_Interface::set_goal)
-        )
+             static_cast<void (STRIPS_Interface::*)(std::vector<std::pair<int, bool>> &)>(&STRIPS_Interface::set_goal))
+        .def("set_goal",
+             static_cast<void (STRIPS_Interface::*)(py::list &)>(&STRIPS_Interface::set_goal))
         .def("set_domain_name", &STRIPS_Interface::set_domain_name)
         .def("set_problem_name", &STRIPS_Interface::set_problem_name)
         .def("write_ground_pddl", &STRIPS_Interface::write_ground_pddl)
@@ -83,44 +84,35 @@ PYBIND11_MODULE(wrapper, m)
         .def("print_fluents", &STRIPS_Interface::print_fluents)
         .def("finalize_actions", &STRIPS_Interface::finalize_actions)
         .def_readwrite("parsing_time", &STRIPS_Interface::m_parsing_time)
-        .def_readwrite("ignore_action_costs", &STRIPS_Interface::m_ignore_action_costs)
-    ;
+        .def_readwrite("ignore_action_costs", &STRIPS_Interface::m_ignore_action_costs);
 
     py::class_<aptk::STRIPS_Problem>(m, "STRIPS_Problem")
-		.def(py::init<std::string, std::string>())
-    ;
+        .def(py::init<std::string, std::string>());
 
     py::class_<Fwd_Search_Problem>(m, "Fwd_Search_Problem")
-        .def(py::init<aptk::STRIPS_Problem*>())
-    ;
+        .def(py::init<aptk::STRIPS_Problem *>());
 
     typedef H1_Callback<STRIPS_Interface, Fwd_Search_Problem, H_Add_Evaluation_Function> H_Add;
 
     py::class_<H_Add>(m, "H_Add")
-        .def(py::init<STRIPS_Interface&>())
+        .def(py::init<STRIPS_Interface &>())
         .def("print_values", &H_Add::print_values)
-        .def("compute_init_h", static_cast<std::string (H_Add::*) 
-                (py::list&, py::list&)> (&H_Add::compute_init_h))
-        .def("compute_init_h", static_cast<std::string (H_Add::*) 
-                ()> (&H_Add::compute_init_h))
+        .def("compute_init_h", static_cast<std::string (H_Add::*)(py::list &, py::list &)>(&H_Add::compute_init_h))
+        .def("compute_init_h", static_cast<std::string (H_Add::*)()>(&H_Add::compute_init_h))
         .def("fetch_relevant_actions", &H_Add::fetch_relevant_actions)
         .def("fetch_supporting_actions", &H_Add::fetch_supporting_actions)
         .def("fetch_best_supporters", &H_Add::fetch_best_supporters)
-        .def("fetch_hval_fluents", &H_Add::fetch_hval_fluents)
-    ;
+        .def("fetch_hval_fluents", &H_Add::fetch_hval_fluents);
 
     typedef H1_Callback<STRIPS_Interface, Fwd_Search_Problem, H_Max_Evaluation_Function> H_Max;
 
     py::class_<H_Max>(m, "H_Max")
-        .def(py::init<STRIPS_Interface&>())
+        .def(py::init<STRIPS_Interface &>())
         .def("print_values", &H_Max::print_values)
-        .def("compute_init_h", static_cast<std::string (H_Max::*) 
-                (py::list&, py::list&)> (&H_Max::compute_init_h))
-        .def("compute_init_h", static_cast<std::string (H_Max::*) 
-                ()> (&H_Max::compute_init_h))
+        .def("compute_init_h", static_cast<std::string (H_Max::*)(py::list &, py::list &)>(&H_Max::compute_init_h))
+        .def("compute_init_h", static_cast<std::string (H_Max::*)()>(&H_Max::compute_init_h))
         .def("fetch_relevant_actions", &H_Max::fetch_relevant_actions)
         .def("fetch_supporting_actions", &H_Max::fetch_supporting_actions)
         .def("fetch_best_supporters", &H_Max::fetch_best_supporters)
-        .def("fetch_hval_fluents", &H_Max::fetch_hval_fluents)
-    ;
+        .def("fetch_hval_fluents", &H_Max::fetch_hval_fluents);
 }
