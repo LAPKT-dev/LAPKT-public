f616e7b49b969f48afce367b61dd24869764d744
Anubhav Singh <anubhav.singh.eng@gmail.com>
2025-03-26 00:03:09 -0400

    PR 0.2.0
    Initial code for version 0.2.0

--- src/python/_package/setup.py
+++ src/python/_package/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lapkt",
-    version="0.1.8",
+    version="0.2.0",
     author="Anubhav Singh, Nir Lipovetzky, "
             + "Miquel Ramirez, Christian Muise",
     author_email="anubhav.singh.er@pm.me, nirlipo@gmail.com, "
